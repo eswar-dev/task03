ec0cadb8de2131960920222c01d6ebab9f5e513c
SILAVERI ESWAR <eswar30082002@gmail.com>
2022-01-11 22:56:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 
 import './App.css';
 import Card from './card';
+
 const App=()=>{
+  
   return(
+    
+    
     <div className='container'>
+      
       <div className='row cont'>
           <Card title="React and Redux Course" image="mobile.png" description="This course contains all type of functionalities...."  progress="good"percentage="60" />
           <Card title="React and prosp" image="mobile.png" description="This course contains all type of functionalities...."progress="good" percentage="60" />
@@ -13,6 +18,7 @@ const App=()=>{
           <Card title="React and ES6" image="mobile.png" description="This course contains all type of functionalities...."progress="good" percentage="60"/>
           <Card title="React and arrow" image="mobile.png" description="This course contains all type of functionalities...."progress="good" percentage="60"/>
       </div>
+      
     </div>
   )
 }
--- src/card.js
+++ src/card.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import bootstrap from 'bootstrap';
+
 
 
 const Card = (props) => {
@@ -9,9 +9,16 @@ const Card = (props) => {
     const percentage = Number(props.percentage)
     console.log(percentage);
     return (
+        
+                        
+        
         <div className="card m-2 col-md-3" >
-            <img src={props.image} className="card-img-top" alt="..."/>
-            <div className="card-body">
+            <div className="card" >
+                        
+           <img src={props.image} className="card-img-top" alt="..."/>
+            <div className="card-body"  >
+                
+                
                 <h5 className="card-title">course title: <h6>{props.title}</h6></h5>
                 <p className="card-text"><h5>Course description:</h5>{props.description}</p>
                 <h5 className="card-progress">course progress: <h6>{props.progress}</h6></h5>
@@ -21,9 +28,14 @@ const Card = (props) => {
                      <CircularProgressbar style={{display:'flex',justifyContent:'center'}} value={percentage} text={`${percentage}%`} />
                 </div>
                 </div>
+                </div>
                 
             </div>
-        </div>
+            </div>
+            
+            
+
+        
     )
 }
 
